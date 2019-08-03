20a6fd85db7115746bb6d982c0f9ef21277644ae
Bob Hiltner <bobhilt@gmail.com>
2019-08-03 09:20:49 -1000

    Update based on reviewing Tony Alicea JS course.

---
+++ WordPress.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1564859388547" ID="ID_1782719574" MODIFIED="1564859395385" TEXT="WordPress">
+<node CREATED="1564859432349" ID="ID_931239211" MODIFIED="1564859434811" POSITION="right" TEXT="tools">
+<node CREATED="1564859434812" ID="ID_1146858903" MODIFIED="1564859445565" TEXT="Local by Flywheel">
+<node CREATED="1564859459995" ID="ID_93390742" LINK="https://localbyflywheel.com/" MODIFIED="1564859497057" TEXT="https://localbyflywheel.com/"/>
+</node>
+<node CREATED="1564859595919" ID="ID_1814223866" MODIFIED="1564859600626" TEXT="Bedrock">
+<node CREATED="1564859602611" ID="ID_1764523447" LINK="https://roots.io/bedrock/" MODIFIED="1564859607009" TEXT="https://roots.io/bedrock/"/>
+</node>
+</node>
+</node>
+</map>
